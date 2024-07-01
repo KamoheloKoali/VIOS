5aafaec52d476b42305197c389c96676321e406e
Kamohelo Koali <152155388+KamoheloKoali@users.noreply.github.com>
2024-07-01 11:00:17 +0200

    Delete landing_page directory

--- landing_page/components/sharedClasses.ts
+++
@@ -1,9 +0,0 @@
-// components/sharedClasses.ts
-export const sharedClasses = {
-    zincBg: 'bg-zinc-100',
-    zincText: 'text-zinc-800',
-    zincHover: 'hover:underline',
-    zincButton: 'bg-zinc-200 text-zinc-800 py-3 px-6 md:py-4 md:px-8 rounded-full hover:bg-zinc-200/80',
-    zincCard: 'bg-zinc-200 text-zinc-800 p-4 rounded-md shadow',
-  };
-  
